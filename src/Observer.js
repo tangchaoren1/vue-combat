3dcb722f5ff7b1c52ee4243d25ccdf9e16df8968
tangchaoren1 <735405280@qq.com>
2017-12-15 09:41:58 +0800

    delete  code

--- src/Observer.js
+++ src/Observer.js
@@ -29,21 +29,21 @@ viewer.walk = function (obj) {
 var target = document.getElementById('observer');
 viewer.convert = function (key, val) {
     var dep = new Dep();
-    
+
     Object.defineProperty(data, key, {
         enumerable: true,
         configurable: true,
         get: function () {
             //把订阅者收集到一个数组中；  
-           Dep.target && dep.addSub(Dep.target);
-           return val; 
+            Dep.target && dep.addSub(Dep.target);
+            return val;
         },
         set: function (newVal) {
             if (newVal != val) {
                 //如果值发生了改变出发订阅者的消息通知器。
                 val = newVal;
                 dep.notify()
-            }          
+            }
         }
     })
 }
@@ -56,9 +56,6 @@ Dep.prototype = {
     addSub: function (sub) {
         this.subs.push(sub)
     },
-    depend: function () {
-        Dep.target.addDep(this);
-    },
     notify: function () {
         this.subs.forEach(function (sub) {
             sub.update();
@@ -68,10 +65,9 @@ Dep.prototype = {
 
 Dep.target = null;
 
-function Watcher(node,key,upFunc) {
-   this.value = this.get(key);
-   this.exfun = upFunc;
-   console.log(this.exfun)
+function Watcher(node, key, upFunc) {
+    this.value = this.get(key);
+    this.exfun = upFunc;
 }
 Watcher.prototype = {
     get: function (key) {
@@ -82,35 +78,35 @@ Watcher.prototype = {
     },
     //通过绑定的回调函数，更新所绑定元素的值。
     update: function () {
-        this.exfun(); 
+        this.exfun();
     }
 }
 
 //从根元素开始遍历，收集元素上的所有信息。
 function Compile(el) {
-  var nodes = el.childNodes;
-  [].slice.call(nodes).forEach((node)=>{
-      //如果是元素
-      if(node.nodeType === 1){
-        this.compile(node)
-      }      
-  })
+    var nodes = el.childNodes;
+    [].slice.call(nodes).forEach((node) => {
+        //如果是元素
+        if (node.nodeType === 1) {
+            this.compile(node)
+        }
+    })
 }
 
 Compile.prototype = {
-    compile:function(node){
+    compile: function (node) {
         //收集元素上绑定的所有属性
-       var nodeAttrs = node.attributes;
-       [].slice.call(nodeAttrs).forEach((attr)=>{
-            this.bind(node,attr)
-       })
+        var nodeAttrs = node.attributes;
+        [].slice.call(nodeAttrs).forEach((attr) => {
+            this.bind(node, attr)
+        })
     },
-    bind: function(node,attr){
+    bind: function (node, attr) {
         //更新元素上的text,并且添加一个回调函数
         var eleValue = attr.value;
-        new Watcher(node,eleValue,function(){
+        new Watcher(node, eleValue, function () {
             node.textContent = data[eleValue];
-        })      
+        })
     }
 
 }
